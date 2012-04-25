70b6ecda443e7d4fb8a6340dd77ca1822bc832e3
Tomaz Muraus <tomaz@tomaz.me>
2012-04-24 17:04:53 -0700

    Add test-travisci target.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,3 +1,9 @@
+if [ $TRAVISCI ]; then
+  config="tests/dependencies-travis.json"
+else
+  config="tests/dependencies.json"
+fi
+
 NODE_PATH=lib node_modules/whiskey/bin/whiskey \
   --tests "tests/test-identity-provider-middleware.js tests/test-authentication-middleware.js tests/test-rate-limiting-middleware.js" \
-  --dependencies tests/dependencies.json --real-time --sequential
+  --dependencies ${config} --real-time --sequential
