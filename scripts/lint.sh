a2be0da648b8d5d86153e76cab324781175902ef
Tomaz Muraus <tomaz@tomaz.me>
2012-11-08 11:41:22 -0800

    Lint the example config.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Lint JavaScript files
 ./node_modules/.bin/jshint $(find ./lib ./tests ./misc -type f -name "*.js") --config jshint.json
+
+# Check example config syntax
+cat example/config.json | python -mjson.tool
