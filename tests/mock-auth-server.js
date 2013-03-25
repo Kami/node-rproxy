185075c09699fa8bf9b7e4ec5042edca8a22e95f
gdusbabek <gdusbabek@gmail.com>
2013-03-25 09:43:28 -0500

    lint

--- tests/mock-auth-server.js
+++ tests/mock-auth-server.js
@@ -125,7 +125,7 @@ function getToken_v2_0(req, res) {
 
     if (creds.tenantName) {
       tenantId = creds.tenantName;
-      // if you wish to use fake accounts, specify a tenant id beginning with 'FAKE' and then supply any token id. 
+      // if you wish to use fake accounts, specify a tenant id beginning with 'FAKE' and then supply any token id.
       if (FAKE_ACCOUNT_REGEX.test(tenantId)) {
         username = tenantId;
         token = creds.token.id;
